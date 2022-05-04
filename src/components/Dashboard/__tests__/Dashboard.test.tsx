14e077a335c9c7f5b8d0d1efece46f2a8fdc3264
Mark Rivera <markoctavio.riverac@gmail.com>
2022-05-04 11:20:40 -0500

    [SETUP] Setup Project

---
+++ src/components/Dashboard/__tests__/Dashboard.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Dashboard from "../Dashboard";
+
+test("renders learn react link", () => {
+  render(<Dashboard />);
+  const linkElement = screen.getByText(/Dashboard/i);
+  expect(linkElement).toBeInTheDocument();
+});
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Home.module.scss";
+
+const Home = () => {
+  return (
+    <div>
+      <h2 className={styles.home}>Home</h2>
+    </div>
+  );
+};
+
+export default Home;
