c8d010b90725887f1eadf1b370035d1b0d8c953e
Mark Rivera <markoctavio.riverac@gmail.com>
2022-05-06 11:52:23 -0500

    feat: [DMP-383] Create component as base for views

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "components/Home";
-import Dashboard from "components/Dashboard";
-import About from "components/About";
-import Navigation from "components/Navigation";
 
 const App = () => {
   return (
     <Router>
-      <Navigation />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/dashboard" element={<Dashboard />}></Route>
       </Routes>
     </Router>
   );
