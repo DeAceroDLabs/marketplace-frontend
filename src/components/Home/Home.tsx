af8d449daf043c60815a0d1be6491503fce4a6a3
Mark Rivera <markoctavio.riverac@gmail.com>
2022-05-06 13:09:27 -0500

    [DMP-383] Use Element instead of FC for props

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,21 +3,17 @@ import React from "react";
 import styles from "./Home.module.scss";
 
 const Home: React.FunctionComponent = () => {
-  const HomeHeader: React.FC = () => (
+  const homeHeader = (
     <h2 className={styles.header}>This is a header for Home</h2>
   );
 
-  const HomeBody: React.FC = () => (
-    <h2 className={styles.body}>This is a body for Home</h2>
-  );
+  const homeBody = <h2 className={styles.body}>This is a body for Home</h2>;
 
-  const HomeFooter: React.FC = () => (
+  const homeFooter = (
     <h2 className={styles.footer}>This is a footer for Home</h2>
   );
 
-  return (
-    <View header={<HomeHeader />} body={<HomeBody />} footer={<HomeFooter />} />
-  );
+  return <View header={homeHeader} body={homeBody} footer={homeFooter} />;
 };
 
 export default Home;
