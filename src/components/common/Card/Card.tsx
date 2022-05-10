7f91013b56cbc160158327ea85449c9e3655d41b
dianabasilio <dianabasilio99@gmail.com>
2022-05-10 16:29:20 -0500

    [DMP-386] Added more props to avoid multiple validations

--- src/components/common/Card/Card.tsx
+++ src/components/common/Card/Card.tsx
@@ -1,47 +1,27 @@
 import styles from "./Card.module.scss";
 
 interface CardProps {
+  title:  string;
   size?: "small" | "medium" | "large";
   children?: React.ReactNode;
+  secondaryInfo?: string
+  imgSrc: string;
 }
 
 const Card: React.FC<CardProps> = ({
+  title,
   children,
   size = "small",
+  imgSrc,
+  secondaryInfo,
 }) => {
-
-  let smallImage = '/card-image.png';
-  let mediumImage = '/medium-card-image.png';
-  let largeImage = '/large-card-image.png';
-
-  if (size == "small"){
-    return (
-
-      <div>
-        <div className={styles.small}>
-          <img className={styles.productImage} src={smallImage} alt="card" />
-          <div className={styles.productInfo}> Malla Graduada Triple Nudo Venadera -XTREME</div>
-        </div>
-      </div>
-    );
-  }
-  else if (size == "medium"){
-    return(
-      <div>
-      <div className={styles.large}>
-        <img className={styles.productImage} src={mediumImage} alt="card" />
-        <div className={styles.productInfo}> Malla Ciclónica Galvanizada</div>
-      </div>
-    </div>
-    );
-  }
   return (
     <div>
-      <div className={styles.large}>
-        <img className={styles.productImage} src={largeImage} alt="card" />
-        <div className={styles.productInfo}>
-        <div className={styles.name}> Grapa Cartonera</div>
-          <div className={styles.price}> $1080-$1200</div>
+      <div className={`${styles.card} ${styles[size]}`}>
+        <img className={styles.cardImage} src={imgSrc} alt="card" />
+        <div className={styles.cardInfo}>
+          <div className={styles.cardName}> {title}</div>
+          <div className={styles.secondaryInfo}> {secondaryInfo}</div>
         </div>
       </div>
     </div>
