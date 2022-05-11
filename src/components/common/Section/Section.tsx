3b873ba9b5e01a414f4c602d5c18f1d3964945f9
Mark Rivera <markoctavio.riverac@gmail.com>
2022-05-11 12:56:43 -0500

    [DMP-382] Add styles to Section component

--- src/components/common/Section/Section.tsx
+++ src/components/common/Section/Section.tsx
@@ -7,10 +7,10 @@ interface SectionProps {
 
 const Section: React.FC<SectionProps> = ({ title, children }) => {
   return (
-    <>
+    <div className={styles.section}>
       <div className={styles.title}> {title} </div>
       <div className={styles.body}> {children} </div>
-    </>
+    </div>
   );
 };
 
