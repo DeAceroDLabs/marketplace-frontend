1e91a2704961672e6f2bcb8dfe08b87ba9f7bf2b
Mark Rivera <markoctavio.riverac@gmail.com>
2022-05-11 11:56:23 -0500

    [DMP-382] Created tabs menu and tabs component

---
+++ src/components/common/TabsMenu/Tab/Tab.tsx
@@ -0,0 +1,23 @@
+import styles from "./Tab.module.scss";
+
+interface TabProps {
+  title: string;
+  variant: "primary" | "secondary";
+  active: "active" | "";
+  onSelect: (tabTitle: string) => void;
+}
+
+const Tab: React.FC<TabProps> = ({ title, variant, active, onSelect }) => {
+  const setActive = () => onSelect(title);
+  return (
+    <button
+      key={title}
+      className={`${styles.button} ${styles[variant]} ${styles[active]}`}
+      onClick={() => setActive()}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default Tab;
--- src/components/common/TabsMenu/TabsMenu.tsx
+++ src/components/common/TabsMenu/TabsMenu.tsx
@@ -1,14 +1,31 @@
-interface TabsProps {
+import { useState } from "react";
+import Tab from "./Tab/Tab";
+
+interface TabsMenuProps {
+  variant?: "primary" | "secondary";
   tabsTitles: string[];
   setActive(tab: string): void;
 }
 
-const TabsMenu: React.FC<TabsProps> = ({ tabsTitles, setActive }) => {
+const TabsMenu: React.FC<TabsMenuProps> = ({
+  variant = "primary",
+  tabsTitles,
+}) => {
+  const [activeTab, setactiveTab] = useState(tabsTitles[0]);
+
+  const setActive = (tabTitle: string) => {
+    setactiveTab(tabTitle);
+  };
+
   const tabs = tabsTitles.map((title: string) => {
     return (
-      <button key={title} className="tab" onClick={() => setActive(title)}>
-        {title}
-      </button>
+      <Tab
+        key={title}
+        variant={variant}
+        title={title}
+        active={activeTab === title ? "active" : ""}
+        onSelect={setActive}
+      />
     );
   });
   return <div className="tabs">{tabs}</div>;
