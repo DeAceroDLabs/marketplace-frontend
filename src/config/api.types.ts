dde3734da4cd850bdbe3ad6ec9444c008b9bad15
Mark Rivera <markoctavio.riverac@gmail.com>
2022-05-13 10:24:14 -0500

    feat: [DMP-385] Get Products from service

--- src/config/api.types.ts
+++ src/config/api.types.ts
@@ -1,9 +1,23 @@
 export const API = {
   categories: "categories",
+  products: (categoryId: number) => `categories/${categoryId}/products`,
 };
 
 export type Category = {
   IdCategoria: string;
   NomCategoria: string;
   UrlImagen: string;
+};
+
+export type Article = {
+  ClaArticulo: string;
+};
+
+export type Product = {
+  ClaProducto: number;
+  NomProducto: string;
+  NomCategoria: string;
+  IdCategoria: number;
+  Articulos: Article[];
+  CategoriaUrlImagen: string;
 };
