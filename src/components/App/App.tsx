edff02e97646aec2e4d93f317c16def86e11b9a0
dianabasilio <dianabasilio99@gmail.com>
2022-05-16 16:07:00 -0500

    feat: [DMP-399] Added a search page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "components/Home";
+import Search from "components/Search";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/search" element={<Search/>}></Route>
       </Routes>
     </Router>
   );
