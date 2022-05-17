487004d249f33a94ca5448adc8db065d719a7138
dianabasilio <dianabasilio99@gmail.com>
2022-05-17 16:44:30 -0500

    feat: [DMP-402] Added link to home by clicking the word categories

--- src/components/ProductsNotFound/ProductsNotFound.tsx
+++ src/components/ProductsNotFound/ProductsNotFound.tsx
@@ -5,8 +5,13 @@ import styles from "./ProductsNotFound.module.scss";
 const Search: React.FunctionComponent = () => {
 
   return (
-    <NotFound title="No encontramos un producto que coincida con tu búsqueda" imgSrc="/not-found.jpg" 
-    info="Navega por las categorías para encontrar un producto similar"/>
+    <NotFound title="No encontramos un producto que coincida con tu búsqueda" imgSrc="/not-found.jpg" >
+      <div>
+      Navega por las <a href='/'>categorías</a> para encontrar un producto similar
+      </div>
+
+    </NotFound>
+
   );
 };
 
