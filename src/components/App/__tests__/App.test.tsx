77c9cf9ee916802a13fd6d96afe3cfda96035f4b
Mark Rivera <markoctavio.riverac@gmail.com>
2022-05-17 13:16:11 -0500

    [DMP-388] Setup tests for App

--- src/components/App/__tests__/App.test.tsx
+++ src/components/App/__tests__/App.test.tsx
@@ -1,9 +1,8 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
 import App from "../App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Dashboard/i);
-  expect(linkElement).toBeInTheDocument();
+it("renders App with no issue", () => {
+  const component = renderer.create(<App />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
