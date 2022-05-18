ba1ab0c30453c996478731817c322a8544729745
dianabasilio <dianabasilio99@gmail.com>
2022-05-18 13:21:44 -0500

    feat: [DMP-402] Adding Beautify and Prettier extensions to the code

--- src/components/Search/ProductsNotFound/ProductsNotFound.tsx
+++ src/components/Search/ProductsNotFound/ProductsNotFound.tsx
@@ -3,15 +3,19 @@ import NotFound from "components/common/NotFound";
 import styles from "./ProductsNotFound.module.scss";
 
 const Search: React.FunctionComponent = () => {
-
   return (
-    <NotFound title="No encontramos un producto que coincida con tu búsqueda" imgSrc="/not-found.jpg" >
+    <NotFound
+      title="No encontramos un producto que coincida con tu búsqueda"
+      imgSrc="/not-found.jpg"
+    >
       <div>
-      Navega por las <a className={styles.a} href='/'>categorías</a> para encontrar un producto similar
+        Navega por las{" "}
+        <a className={styles.a} href="/">
+          categorías
+        </a>{" "}
+        para encontrar un producto similar
       </div>
-
     </NotFound>
-
   );
 };
 
