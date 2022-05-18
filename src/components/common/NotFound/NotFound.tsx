6eae7af4a5767e2e09916a502c25c9a859743ca2
dianabasilio <dianabasilio99@gmail.com>
2022-05-18 13:11:37 -0500

    feat: [DMP-402] Button can also be part of children prop

--- src/components/common/NotFound/NotFound.tsx
+++ src/components/common/NotFound/NotFound.tsx
@@ -21,8 +21,6 @@ const NotFound: React.FC<NotFoundProps> = ({
         <div className={styles.containerInfo}>
           <div className={styles.title}>{title}</div>
           <div className={styles.info}>{children}</div>
-          {/* Button won´t be showed if buttonInfo prop is not passed */}
-          {buttonInfo && <button className={styles.buttonInfo}>{buttonInfo}</button>}
         </div>
       </div>
     </div>
