4e97c05dcb38d7b78c59f5f645fac382ad7c5185
dianabasilio <dianabasilio99@gmail.com>
2022-05-18 13:07:20 -0500

    feat: [DMP-402] ProductsNotFound component inside /Search, deleted route /productsnotfound because it is not needed

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -2,16 +2,27 @@ import React from "react";
 import Section from "components/common/Section";
 import View from "components/common/View";
 import styles from "./Search.module.scss";
+import ProductsNotFound from "./ProductsNotFound";
+import { useState } from "react";
 
 const Search: React.FunctionComponent = () => {
 
+    // state
+  const [items] = useState([])
+
+  // Node
+  {if (items.length) {
   return (
     <View >
       <Section title="Search page">
         <div className={styles.body}> Search bar </div>
       </Section>
     </View>
-  );
+  ); // code for displaying cards (empty for now)
+  }}
+
+  return <ProductsNotFound />
+
 };
 
 export default Search;
