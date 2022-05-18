6b1faef8f0c740998239f681c36b4e439f9f427f
dianabasilio <dianabasilio99@gmail.com>
2022-05-18 14:03:08 -0500

    feat: [DMP-402] Deleting not needed comments and lines

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -6,10 +6,8 @@ import ProductsNotFound from "./ProductsNotFound";
 import { useState } from "react";
 
 const Search: React.FunctionComponent = () => {
-  // state
   const [items] = useState([]);
 
-  // Node
   {
     if (items.length) {
       return (
@@ -18,7 +16,7 @@ const Search: React.FunctionComponent = () => {
             <div className={styles.body}> Search bar </div>
           </Section>
         </View>
-      ); // code for displaying cards (empty for now)
+      );
     }
   }
 
