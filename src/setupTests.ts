8f9fb1bba2d959f350a6f01de66b421eba175cf6
Mark Rivera <markoctavio.riverac@gmail.com>
2022-05-18 19:11:28 -0500

    [DMP-388] Added tests for Home component

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,64 @@
+import { API, Category, Product } from "./config/api.types";
 import "@testing-library/jest-dom";
+
+const getProductsByCategory = (categoryId: number) => {
+  return [
+    {
+      ClaProducto: 0,
+      NomProducto: `category ${categoryId} product 1`,
+      NomCategoria: "mock category 1",
+      IdCategoria: categoryId,
+      Articulos: [
+        {
+          ClaArticulo: "mock article 1",
+        },
+      ],
+      CategoriaUrlImagen: "https://picsum.photos/200/300",
+    },
+  ];
+};
+
+const categoriesResponse: Category[] = [
+  {
+    IdCategoria: 1,
+    UrlImagen: "https://picsum.photos/200/300",
+    NomCategoria: "mock category 1",
+  },
+  {
+    IdCategoria: 2,
+    UrlImagen: "https://picsum.photos/200/300",
+    NomCategoria: "mock category 2",
+  },
+];
+
+const productsResponse: Product[] = getProductsByCategory(1);
+const productsResponse2: Product[] = getProductsByCategory(2);
+
+export default async function mockFetch(url: RequestInfo): Promise<Response> {
+  switch (url) {
+    case API.categories: {
+      return {
+        ok: true,
+        status: 200,
+        json: async () => categoriesResponse,
+      } as Response;
+    }
+    case API.products(1): {
+      return {
+        ok: true,
+        status: 200,
+        json: async () => productsResponse,
+      } as Response;
+    }
+    case API.products(2): {
+      return {
+        ok: true,
+        status: 200,
+        json: async () => productsResponse2,
+      } as Response;
+    }
+    default: {
+      throw new Error(`Unhandled request: ${url}`);
+    }
+  }
+}
