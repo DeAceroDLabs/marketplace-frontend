adcbcac72b3dc42e0b8769ecac63153ae4b2546f
Mark Rivera <markoctavio.riverac@gmail.com>
2022-05-18 19:11:28 -0500

    [DMP-388] Added tests for Home component

--- src/components/Home/__tests__/Home.test.tsx
+++ src/components/Home/__tests__/Home.test.tsx
@@ -1,8 +1,36 @@
-import renderer from "react-test-renderer";
+import { render, waitFor, screen, fireEvent } from "@testing-library/react";
+import { debug } from "console";
+import mockFetch from "setupTests";
 import Home from "../Home";
 
-it("renders Home with no issue", () => {
-  const component = renderer.create(<Home />);
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+describe("Home", () => {
+  beforeEach(() => {
+    jest.spyOn(window, "fetch").mockImplementation(mockFetch);
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  const loadInitialState = async () => {
+    render(<Home />);
+    await waitFor(() => {
+      expect(screen.getByText("mock category 1")).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(screen.getByText("category 1 product 1")).toBeInTheDocument();
+    });
+  };
+
+  it("loads categories into screen", async () => {
+    loadInitialState();
+  });
+
+  it("loads products from selected category", async () => {
+    await loadInitialState();
+    fireEvent.click(screen.getByText("mock category 2"));
+    await waitFor(() => {
+      expect(screen.getByText("category 2 product 1")).toBeInTheDocument();
+    });
+  });
 });
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -2,26 +2,15 @@ import React from "react";
 import Section from "components/common/Section";
 import View from "components/common/View";
 import styles from "./Search.module.scss";
-import ProductsNotFound from "./ProductsNotFound";
-import { useState } from "react";
-import { Product } from "config/api.types";
 
 const Search: React.FunctionComponent = () => {
-  const [products, setProducts] = useState<Product[]>([]);
-
-  {
-    if (products.length) {
-      return (
-        <View>
-          <Section title="Search page">
-            <div className={styles.body}> Search bar </div>
-          </Section>
-        </View>
-      );
-    }
-  }
-
-  return <ProductsNotFound />;
+  return (
+    <View>
+      <Section title="Search page">
+        <div className={styles.body}> Search bar </div>
+      </Section>
+    </View>
+  );
 };
 
 export default Search;
