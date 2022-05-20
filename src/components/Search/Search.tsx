9a309592f7826ff4d4c1b4d42638a8faa66e2dbd
dianabasilio <dianabasilio99@gmail.com>
2022-05-20 12:27:41 -0500

    [DMP-397] Added query by enter click and button click

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -2,15 +2,26 @@ import React from "react";
 import Section from "components/common/Section";
 import View from "components/common/View";
 import styles from "./Search.module.scss";
+import ProductsNotFound from "./ProductsNotFound";
+import { useState } from "react";
+import { Product } from "config/api.types";
 
 const Search: React.FunctionComponent = () => {
-  return (
-    <View>
-      <Section title="Search page">
-        <div className={styles.body}> Search bar </div>
-      </Section>
-    </View>
-  );
+  const [products, setProducts] = useState<Product[]>([]);
+
+  {
+    if (products.length) {
+      return (
+        <View>
+          <Section title="Search page">
+            <div className={styles.body}> Search bar </div>
+          </Section>
+        </View>
+      );
+    }
+  }
+
+  return <ProductsNotFound />;
 };
 
 export default Search;
--- src/components/common/SearchBar/SearchBar.tsx
+++ src/components/common/SearchBar/SearchBar.tsx
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./SearchBar.module.scss";
 
 const SearchBar: React.FunctionComponent = () => {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const navigateToSearchPage = () => navigate(`search/${query}`);
+
+  const _handleKeyDown = (e: any) => {
+    if (e.key === 'Enter') {
+      navigateToSearchPage();
+    }
+  }
+  
   return (
     <div className={styles.searchContainer}>
-      <form action="/" method="POST">
-        <div className={styles.searchBar}>
-          <input
-            type="text"
-            placeholder="¿Qué estás buscando?"
-            name="search"
-            id="site-search"
-          />
-          <button type="submit">
-            <i className="fa fa-search"></i>
-          </button>
-        </div>
-      </form>
+      <div className={styles.searchBar}>
+        <input
+          type="text"
+          placeholder="¿Qué estás buscando?"
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          onKeyDown={_handleKeyDown}
+        />
+        <button onClick={navigateToSearchPage}>
+          <i className="fa fa-search"></i>
+        </button>
+      </div>
     </div>
   );
 };
