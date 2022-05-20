19bc562ab0564bb0ee86db49e175a6c8cd19dabe
dianabasilio <dianabasilio99@gmail.com>
2022-05-20 13:15:24 -0500

    [DMP-397] Deleted form tag

--- src/components/common/SearchBar/SearchBar.tsx
+++ src/components/common/SearchBar/SearchBar.tsx
@@ -4,19 +4,17 @@ import styles from "./SearchBar.module.scss";
 const SearchBar: React.FunctionComponent = () => {
   return (
     <div className={styles.searchContainer}>
-      <form action="/" method="POST">
-        <div className={styles.searchBar}>
-          <input
-            type="text"
-            placeholder="¿Qué estás buscando?"
-            name="search"
-            id="site-search"
-          />
-          <button type="submit">
-            <i className="fa fa-search"></i>
-          </button>
-        </div>
-      </form>
+      <div className={styles.searchBar}>
+        <input
+          type="text"
+          placeholder="¿Qué estás buscando?"
+          name="search"
+          id="site-search"
+        />
+        <button type="submit">
+          <i className="fa fa-search"></i>
+        </button>
+      </div>
     </div>
   );
 };
