8073debec591411c385842bca0964f5687d78394
dianabasilio <dianabasilio99@gmail.com>
2022-05-23 15:55:53 -0500

    feat: [DMP-407] Added validations to display, if not in home it displays a double searchbar mounted

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,9 +7,10 @@ const Header: React.FC = () => {
   const activePath = location.pathname;
   const pathIsHome = activePath === "/";
   const backgroundColor = pathIsHome ? "clean" : "color";
+  const searchBarComponent = pathIsHome ? "doNotShowSearchBar" : "showSearchBar";
   return(
     <div className={`${styles.header} ${styles[backgroundColor]}`}>
-      <div className={styles.search}>
+      <div className={styles[searchBarComponent]}>
         <SearchBar />
       </div>
     </div>
