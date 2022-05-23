3d83804500f2814c0758728a310d6972318c87b4
dianabasilio <dianabasilio99@gmail.com>
2022-05-23 13:18:33 -0500

    feat: [DMP-407] Added Search bar on header.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,20 @@
 import { useLocation } from "react-router-dom";
 import styles from "./Header.module.scss";
+import SearchBar from "components/common/SearchBar";
 
 const Header: React.FC = () => {
   const location = useLocation();
   const activePath = location.pathname;
   const pathIsHome = activePath === "/";
   const backgroundColor = pathIsHome ? "clean" : "color";
-  return <div className={`${styles.header} ${styles[backgroundColor]}`} />;
+  return(
+    <div className={`${styles.header} ${styles[backgroundColor]}`}>
+      <div className={styles.search}>
+        <SearchBar />
+      </div>
+    </div>
+  );
+  
 };
 
 export default Header;
