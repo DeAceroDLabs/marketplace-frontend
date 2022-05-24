7c73a88ca6e72410609c7497923b2bcf1af4fb65
Mark Rivera <markoctavio.riverac@gmail.com>
2022-05-24 13:18:10 -0500

    feat: [DMP-398] Creaded handler for POST request

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,5 @@
 import { API, Category, Product } from "./api.types";
+import axios from "axios";
 
 const fetchData = (url: string) => {
   async function fetchUrl() {
@@ -9,6 +10,12 @@ const fetchData = (url: string) => {
   return fetchUrl();
 };
 
+const postRequest = async (url: string, body: any) => {
+  const headers = {};
+  const response = await axios.post(url, body, { headers });
+  return response;
+};
+
 const fetchCategories = (): Promise<Category[]> => {
   return fetchData(API.categories);
 };
@@ -17,4 +24,11 @@ const fetchProducts = (categoryId: number): Promise<Product[]> => {
   return fetchData(API.products(categoryId));
 };
 
-export { fetchCategories, fetchProducts };
+const searchProducts = (query: string): Promise<any> => {
+  const body = {
+    q: query,
+  };
+  return postRequest(API.search, body);
+};
+
+export { fetchCategories, fetchProducts, searchProducts };
