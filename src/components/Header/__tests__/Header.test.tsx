61a22bbc0a7c8aa09d3db9f6b68a3301c3fcc8e9
dianabasilio <dianabasilio99@gmail.com>
2022-05-24 15:40:43 -0500

    [DMP-407] Added Unit test does not display search bar on home screen

--- src/components/Header/__tests__/Header.test.tsx
+++ src/components/Header/__tests__/Header.test.tsx
@@ -1,6 +1,7 @@
 import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import Header from "../Header";
+import { screen } from '@testing-library/react';
 
 describe("Header", () => {
   it("renders Header without problem", () => {
@@ -12,12 +13,22 @@ describe("Header", () => {
     expect(view).toMatchSnapshot();
   });
 
-  it("renders Header with color being on a different page and not double search bar on home", () => {
+  it("renders Header with color being on a different page", () => {
     const view = render(
       <MemoryRouter initialEntries={["/search/mock"]}>
         <Header />
       </MemoryRouter>
     );
     expect(view).toMatchSnapshot();
+  });
+
+  it("does not display search bar on home screen", () => {
+    const view = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Header />
+      </MemoryRouter>
+    );
+    const searchBar = screen.queryByPlaceholderText("¿Qué estás buscando?");
+    expect(searchBar).not.toBeInTheDocument();
   });
 });
