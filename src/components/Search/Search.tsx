6d35f39f4e23306c647882d4234a3e47d56525be
dianabasilio <dianabasilio99@gmail.com>
2022-05-26 09:14:43 -0500

    feat: [DMP-400] From the url send a request to get the items using the search service

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -3,22 +3,54 @@ import Section from "components/common/Section";
 import View from "components/common/View";
 import styles from "./Search.module.scss";
 import ProductsNotFound from "./ProductsNotFound";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Product } from "config/api.types";
+import { searchProducts } from "config/api";
+import { useLocation } from "react-router-dom";
 
 const Search: React.FunctionComponent = () => {
-  const [products] = useState<Product[]>([]);
+  const location = useLocation();
+  const [spinner, setSpinner] = useState(false);
+  const query = location.pathname.split("/").pop() || "";
+  const [products, setProducts] = useState<Product[]>([]);
 
-  if (products.length) {
+  console.log(query);
+
+  useEffect(() => {
+    setSpinner(true);
+    searchProducts(query).then((response) => {
+      setSpinner(false);
+      setProducts(response.data);
+    });
+  }, [query]);
+
+  if (products.length > 0) {
     return (
       <View>
-        <Section title="Search page">
-          <div className={styles.body}> Search bar </div>
-        </Section>
+        {spinner && <div className={styles.title}> <div className={styles.query}>Loading products ..</div> </div>}
+        {!spinner && (
+          <div className={styles.title}>
+            <div className={styles.query}>“{query}”</div>{" "}
+            <div className={styles.results}>
+              {" "}
+              "{products.length} resultados"
+            </div>
+          </div>
+        )}
       </View>
     );
   }
-  return <ProductsNotFound />;
+
+  return (
+    <div>
+      {spinner && (
+        <div className={styles.title}>
+          <div className={styles.query}>Loading products ..</div>
+        </div>
+      )}
+      {!spinner && <ProductsNotFound />}
+    </div>
+  );
 };
 
 export default Search;
