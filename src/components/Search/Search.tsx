ba2036c12df054a4bc8b8f96a16e82a4af5ca36c
dianabasilio <dianabasilio99@gmail.com>
2022-05-26 14:01:33 -0500

    [DMP-400] Deleted repeated and not used code

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,22 +1,19 @@
 import React from "react";
-import Section from "components/common/Section";
 import View from "components/common/View";
 import styles from "./Search.module.scss";
 import ProductsNotFound from "./ProductsNotFound";
 import { useState, useEffect } from "react";
 import { Product } from "config/api.types";
 import { searchProducts } from "config/api";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const Search: React.FunctionComponent = () => {
   const [loading, setLoading] = useState(false);
   const { query } = useParams() || "";
   const [products, setProducts] = useState<Product[]>([]);
 
-  console.log(query);
-
   useEffect(() => {
-    setProducts([])
+    setProducts([]);
     setLoading(true);
     searchProducts(query).then((response) => {
       setProducts(response.data);
@@ -24,34 +21,22 @@ const Search: React.FunctionComponent = () => {
     });
   }, [query]);
 
+  if (loading) {
+    return <div className={styles.query}>Cargando productos...</div>;
+  }
+
   if (products.length > 0) {
     return (
       <View>
-        {loading && (
-          <div className={styles.title}>
-            <div className={styles.query}>Cargando productos...</div>
-          </div>
-        )}
-        {!loading && (
-          <div className={styles.title}>
-            <div className={styles.query}>“{query}”</div>
-            <div className={styles.results}>"{products.length} resultados"</div>
-          </div>
-        )}
+        <div className={styles.title}>
+          <div className={styles.query}>“{query}”</div>
+          <div className={styles.results}>"{products.length} resultados"</div>
+        </div>
       </View>
     );
   }
 
-  return (
-    <div>
-      {loading && (
-        <div className={styles.title}>
-          <div className={styles.query}>Cargando productos...</div>
-        </div>
-      )}
-      {!loading && <ProductsNotFound />}
-    </div>
-  );
+  return <ProductsNotFound />;
 };
 
 export default Search;
