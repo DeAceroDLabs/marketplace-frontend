83b82fd92449c3bde9aa1656e06631e230cf0752
dianabasilio <dianabasilio99@gmail.com>
2022-05-26 17:20:28 -0500

    [DMP-400] Unit tests passed using useParams and waitfor

--- src/components/Search/__tests__/Search.test.tsx
+++ src/components/Search/__tests__/Search.test.tsx
@@ -1,48 +1,19 @@
 import Search from "../Search";
 import axios from "axios";
 import { screen, render, waitFor } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
+import Router, { MemoryRouter } from "react-router-dom";
 import { productsResponse } from "setupTests";
 
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useParams: jest.fn(),
+}));
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe("Search", () => {
-  it("renders loading", async () => {
-    render(
-      <MemoryRouter>
-        <Search />
-      </MemoryRouter>
-    );
-    expect(screen.getByText("Cargando productos...")).toBeInTheDocument();
-  });
-
-  it("renders when found products", async () => {
-    mockedAxios.post.mockResolvedValue({
-      data: [productsResponse],
-    });
-    render(
-      <MemoryRouter initialEntries={["/search/mock"]}>
-        <Search />
-      </MemoryRouter>
-    );
-    await (() => {
-      expect(screen.getByText(/resultados/)).toBeInTheDocument();
-    });
-  });
-
-  it("renders when Not found products", async () => {
-    mockedAxios.post.mockResolvedValue({
-      data: [],
-    });
-    render(
-      <MemoryRouter initialEntries={["/search/mock"]}>
-        <Search />
-      </MemoryRouter>
-    );
-    await (() => {
-      expect(screen.getByText(/No encontramos/)).toBeInTheDocument();
-    });
+  beforeEach(() => {
+    jest.spyOn(Router, "useParams").mockReturnValue({ query: "1234" });
   });
 
   it("renders when loading and then results", async () => {
@@ -54,10 +25,10 @@ describe("Search", () => {
         <Search />
       </MemoryRouter>
     );
-    await (() => {
+    await waitFor(() => {
       expect(screen.getByText(/Cargando/)).toBeInTheDocument();
     });
-    await (() => {
+    await waitFor(() => {
       expect(screen.getByText(/resultados/)).toBeInTheDocument();
     });
   });
@@ -71,10 +42,10 @@ describe("Search", () => {
         <Search />
       </MemoryRouter>
     );
-    await (() => {
+    await waitFor(() => {
       expect(screen.getByText(/Cargando/)).toBeInTheDocument();
     });
-    await (() => {
+    await waitFor(() => {
       expect(screen.getByText(/No encontramos/)).toBeInTheDocument();
     });
   });
