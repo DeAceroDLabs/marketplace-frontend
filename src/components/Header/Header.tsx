bccefc1c9eccd49ed90e761bc085f61d8c6dbad2
dianabasilio <dianabasilio99@gmail.com>
2022-05-30 09:40:03 -0500

    [DMP-405] Header user cart icon, logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router-dom";
-import SearchBar from "components/common/SearchBar";
 import styles from "./Header.module.scss";
+import SearchBar from "components/common/SearchBar";
 
 const Header: React.FC = () => {
   const location = useLocation();
@@ -9,7 +9,26 @@ const Header: React.FC = () => {
   const backgroundColor = pathIsHome ? "clean" : "color";
   return (
     <div className={`${styles.header} ${styles[backgroundColor]}`}>
+      <div className={styles["logo-container"]}>
+        <a href="/"><div id="logo" className={styles.logo}>
+          Logo
+        </div></a>
+      </div>
       {!pathIsHome && <SearchBar />}
+      <div className={styles["icon-container"]}>
+        <a href="/login">
+          <i id="user-icon" className="fa fa-user"></i>
+        </a>
+        <div className={styles.login}>
+          Hola,&nbsp;
+          <b>
+            <a href="/login">Ingresa</a>
+          </b>
+        </div>
+        <span id="shopping-cart" className="material-symbols-outlined">
+          shopping_cart
+        </span>
+      </div>
     </div>
   );
 };
