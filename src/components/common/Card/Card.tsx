b2c047c2e45c9bd8620abb784f661c43530c9d0f
dianabasilio <dianabasilio99@gmail.com>
2022-06-01 18:22:19 -0500

    feat:[DMP-417] Added loading cards to home page

--- src/components/common/Card/Card.tsx
+++ src/components/common/Card/Card.tsx
@@ -3,20 +3,25 @@ import styles from "./Card.module.scss";
 interface CardProps {
   title: string;
   size?: "small" | "medium" | "large";
+  loading?: "loading-true" | "loading-false";
   secondaryInfo?: string;
-  imgSrc: string;
+  imgSrc?: string;
 }
 
 const Card: React.FC<CardProps> = ({
   title,
   size = "small",
+  loading = "loading-false",
   imgSrc,
   secondaryInfo,
 }) => {
   return (
     <div>
-      <div className={`${styles.card} ${styles[size]}`}>
-        <img className={styles.cardImage} src={imgSrc} alt="card" />
+      <div className={`${styles.card} ${styles[size]} ${styles[loading]}`}>
+        {(loading === "loading-false") && (
+          <img className={styles.cardImage} src={imgSrc} alt="card" />
+        )}
+        {loading === "loading-true" && <div className={styles.cardImage} />}
         <div className={styles.cardInfo}>
           <div className={styles.cardName}> {title}</div>
           <div className={styles.secondaryInfo}> {secondaryInfo}</div>
