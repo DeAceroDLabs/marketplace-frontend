c49f3f8cf0b4d20541181c46c9a3d75b24ce5120
Mark Rivera <markoctavio.riverac@gmail.com>
2022-06-01 14:20:39 -0500

    feat: [DMP-429] Add Login page

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,33 @@
+import React, { useContext, useState } from "react";
+import Section from "components/common/Section";
+import View from "components/common/View";
+import UserContext from "config/userContext";
+import { useNavigate } from "react-router-dom";
+
+const Login: React.FunctionComponent = () => {
+  const navigate = useNavigate();
+  const { username, setUser } = useContext(UserContext);
+  const [user, setUsername] = useState(username);
+  const submitUser = () => {
+    setUser(user);
+    navigate(`home`);
+  };
+  return (
+    <View>
+      <Section title="Login">
+        <form onSubmit={submitUser}>
+          <input
+            value={user}
+            type="text"
+            placeholder="username"
+            onChange={(e) => setUsername(e.currentTarget.value)}
+          />
+          <input type="text" placeholder="password" />
+          <button type="submit">Log In</button>
+        </form>
+      </Section>
+    </View>
+  );
+};
+
+export default Login;
