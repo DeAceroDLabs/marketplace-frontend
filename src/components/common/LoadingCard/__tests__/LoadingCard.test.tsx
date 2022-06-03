dd109715e7bfcbe3df6580a420e1f0df83389011
dianabasilio <dianabasilio99@gmail.com>
2022-06-03 12:25:42 -0500

    [DMP-417] Added unit test for loadingcard component

--- src/components/common/LoadingCard/__tests__/LoadingCard.test.tsx
+++ src/components/common/LoadingCard/__tests__/LoadingCard.test.tsx
@@ -1,2 +1,8 @@
 import renderer from "react-test-renderer";
+import LoadingCard from "../LoadingCard";
 
+it("renders LoadingCards with no issue", () => {
+  const component = renderer.create(<LoadingCard cards={6} variant="small" />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
