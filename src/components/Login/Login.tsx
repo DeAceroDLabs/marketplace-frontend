74b082b30b72a0a124a020c70a3b244c2c6e3776
Mark Rivera <markoctavio.riverac@gmail.com>
2022-06-06 09:12:46 -0600

    [DMP-429] Added Unit Testing

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,20 +1,23 @@
 import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Section from "components/common/Section";
 import View from "components/common/View";
 import UserContext from "config/userContext";
-import { Navigate, useNavigate } from "react-router-dom";
 
 const Login: React.FunctionComponent = () => {
   const navigate = useNavigate();
   const { username, setUser } = useContext(UserContext);
   const [user, setUsername] = useState(username);
+
   const submitUser = () => {
     setUser(user);
     navigate(`../`);
   };
+
   if (username) {
-    return <Navigate to={"/"} />;
+    navigate(`../`);
   }
+
   return (
     <View>
       <Section title="Login">
