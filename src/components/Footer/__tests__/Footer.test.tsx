048b1955f84610d44171260209485d83498cc0b9
dianabasilio <dianabasilio99@gmail.com>
2022-06-08 16:42:22 -0500

    [DMP-412] Updated basic unit test

--- src/components/Footer/__tests__/Footer.test.tsx
+++ src/components/Footer/__tests__/Footer.test.tsx
@@ -1,8 +1,13 @@
-import { render, screen, fireEvent,waitFor } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import Footer from "../Footer";
+import { UserProvider } from "config/userContext";
 
 const mockedUsedNavigate = jest.fn();
+const mockUSer = {
+  username: "user",
+  setUser: jest.fn(),
+};
 
 jest.mock("react-router-dom", () => ({
   ...(jest.requireActual("react-router-dom") as any),
@@ -10,33 +15,42 @@ jest.mock("react-router-dom", () => ({
 }));
 
 describe("Footer", () => {
-  it("renders Footer without problem", async() => {
+  it("renders Footer without problem", async () => {
     const view = render(
-      <MemoryRouter>
-        <Footer />
-      </MemoryRouter>
+      <UserProvider value={mockUSer}>
+        <MemoryRouter>
+          <Footer />
+        </MemoryRouter>
+      </UserProvider>
     );
     expect(view).toMatchSnapshot();
   });
 
-  it("renders contact on Footer without problem", () => {
-    const view = render(
-      <MemoryRouter initialEntries={["/search/mock"]}>
-        <Footer />
-      </MemoryRouter>
-    );
-    const contact = screen.getByText("Contacto");
-    expect(contact).toBeInTheDocument();
-  });
-
-  it("renders about on Footer without problem", () => {
+  it("renders contact on Footer without problem", async () => {
     render(
-      <MemoryRouter initialEntries={["/"]}>
-        <Footer />
-      </MemoryRouter>
+      <UserProvider value={mockUSer}>
+        <MemoryRouter>
+          <Footer />
+        </MemoryRouter>
+      </UserProvider>
     );
-    const about = screen.getByText("Acerca de");
-    expect(about).toBeInTheDocument();
+    await waitFor (() => {
+      const contact = screen.getByText(/Contacto/);
+      expect(contact).toBeInTheDocument();
+    });
   });
 
+  it("renders about on Footer without problem", async () => {
+    render(
+      <UserProvider value={mockUSer}>
+        <MemoryRouter>
+          <Footer />
+        </MemoryRouter>
+      </UserProvider>
+    );
+    await waitFor (() => {
+      const about = screen.getByText("Acerca de");
+      expect(about).toBeInTheDocument();
+    });
+  });
 });
