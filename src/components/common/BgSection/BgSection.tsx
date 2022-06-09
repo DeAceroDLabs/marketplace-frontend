99c4ae30d53aaff945f919494cd3eeea2149d1e4
Mark Rivera <markoctavio.riverac@gmail.com>
2022-06-09 12:15:04 -0500

    [DMP-30] Updated BgSection

--- src/components/common/BgSection/BgSection.tsx
+++ src/components/common/BgSection/BgSection.tsx
@@ -4,17 +4,23 @@ interface BgSectionProps {
   bgImage?: string;
   orientation?: "vertical" | "horizontal";
   children?: React.ReactNode;
+  color?: "primary" | "secondary";
+  position?: "left" | "right";
 }
 
 const BgSection: React.FC<BgSectionProps> = ({
   children,
   bgImage,
+  position = "left",
+  color = "transparent",
   orientation = "horizontal",
 }) => {
   return (
     <div className={`${styles.container} ${styles[orientation]}`}>
       {bgImage && <img src={bgImage} className={`${styles.bgImage}`} alt="" />}
-      <div className={styles.content}>{children}</div>
+      <div className={`${styles.content} ${styles[color]} ${styles[position]}`}>
+        {children}
+      </div>
     </div>
   );
 };
