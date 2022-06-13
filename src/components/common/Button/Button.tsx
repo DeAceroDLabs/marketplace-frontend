2953ad62697756151bea504342794328979a2e31
dianabasilio <dianabasilio99@gmail.com>
2022-06-13 17:22:11 -0500

    [DMP-434] Added primary large css

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -2,7 +2,7 @@ import styles from "./Button.module.scss";
 
 interface ButtonProps {
   children?: React.ReactNode;
-  color?: "primary" | "transparent" | "transparent-black" | "white";
+  color?: "primary" | "primary-large" | "transparent" | "transparent-black" | "white";
   action: () => void;
 }
 
