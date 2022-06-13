8fcfa7011e4d4e79494693d6b7b95df446eb522a
Mark Rivera <markoctavio.riverac@gmail.com>
2022-06-13 11:29:18 -0500

    [DMP-430] Added variant optional to Section

--- src/components/common/Section/Section.tsx
+++ src/components/common/Section/Section.tsx
@@ -3,10 +3,14 @@ import styles from "./Section.module.scss";
 interface SectionProps {
   title?: string | React.ReactNode;
   children: React.ReactNode;
-  variant: "margin-title" | "no-margin-title";
+  variant?: "margin-title" | "no-margin-title";
 }
 
-const Section: React.FC<SectionProps> = ({ title, children, variant }) => {
+const Section: React.FC<SectionProps> = ({
+  title,
+  children,
+  variant = "margin-title",
+}) => {
   return (
     <div className={styles.section}>
       <div className={styles[variant]}> {title} </div>
