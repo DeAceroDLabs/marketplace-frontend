33651c83570d2f95194d14a88116377e4363a986
dianabasilio <dianabasilio99@gmail.com>
2022-06-23 11:12:40 -0500

    [DMP-435] Added a variable that contains tooltip content

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -25,11 +25,36 @@ const Header: React.FC = () => {
   const tooltip = useRef<any>();
   const cartButton = useRef<any>();
   const closeTooltip = (e: any) => {
-    if (tooltip.current && cartButton.current  && isOpen && !tooltip.current.contains(e.target) && !cartButton.current.contains(e.target)) {
+    if (isOpen && !tooltip.current.contains(e.target) && !cartButton.current.contains(e.target)) {
       setOpen(false);
     }
   };
   document.addEventListener("mousedown", closeTooltip);
+
+  const tooltipContent = (
+    <div className={styles["tooltip-children"]}>
+    <div className={styles["tooltip-text"]}>
+      <p>¡No tienes artículos en tu carrito! </p>
+    </div>
+    <img
+      className={styles["tooltip-img"]}
+      src="/empty-cart.jpg"
+      alt="empty-cart"
+    />
+    <div className={styles["tooltip-button"]}>
+      <Button
+        color="primary"
+        action={() => {
+          navigate("cart");
+          toggleTooltip();
+        }}
+      >
+        <div>Agrega productos al carrito</div>
+      </Button>
+    </div>
+  </div>
+  );
+
 
   if (!username) {
     return null;
@@ -62,27 +87,7 @@ const Header: React.FC = () => {
       {isOpen && (
         <div className={styles.tooltip} ref={tooltip}>
           <Tooltip title="Mi carrito" action={() => toggleTooltip()}>
-            <div className={styles["tooltip-children"]}>
-              <div className={styles["tooltip-text"]}>
-                <p>¡No tienes artículos en tu carrito! </p>
-              </div>
-              <img
-                className={styles["tooltip-img"]}
-                src="/empty-cart.jpg"
-                alt="empty-cart"
-              />
-              <div className={styles["tooltip-button"]}>
-                <Button
-                  color="primary"
-                  action={() => {
-                    navigate("cart");
-                    toggleTooltip();
-                  }}
-                >
-                  <div>Agrega productos al carrito</div>
-                </Button>
-              </div>
-            </div>
+            {tooltipContent}
           </Tooltip>
         </div>
       )}
