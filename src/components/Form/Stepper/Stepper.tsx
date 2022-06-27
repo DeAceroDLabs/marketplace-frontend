cf76be0afe3e67ff3a40c12878e2783bfecab53e
Mark Rivera <markoctavio.riverac@gmail.com>
2022-06-27 16:47:10 -0500

    [DMP-426] Add title to stepper

--- src/components/Form/Stepper/Stepper.tsx
+++ src/components/Form/Stepper/Stepper.tsx
@@ -4,22 +4,26 @@ import Step from "./Step/Step";
 import styles from "./Stepper.module.scss";
 
 interface StepperProps {
+  title: string;
   steps: string[];
 }
 
-const Steps: React.FC<StepperProps> = ({ steps }) => {
+const Steps: React.FC<StepperProps> = ({ steps, title }) => {
   const { activeForm } = useContext(MultiFormContext);
   return (
-    <div className={styles.stepper}>
-      <div className={styles["stepper-selector"]}>
-        <div className={styles.line} />
-        {steps.map((step) => (
-          <Step
-            key={step}
-            step={step}
-            isSelected={activeForm.formTitle === step}
-          />
-        ))}
+    <div className={styles["stepper-wrapper"]}>
+      <div className={styles.stepper}>
+        <div className={styles.title}>{title}</div>
+        <div className={styles["stepper-selector"]}>
+          <div className={styles.line} />
+          {steps.map((step) => (
+            <Step
+              key={step}
+              step={step}
+              isSelected={activeForm.formTitle === step}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
