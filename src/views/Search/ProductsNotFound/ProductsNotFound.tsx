0e1f44eac3596108360573ce347779719b727fcd
dianabasilio <dianabasilio99@gmail.com>
2022-06-27 10:24:04 -0500

    [DMP-397] Button for categories navigation

--- src/views/Search/ProductsNotFound/ProductsNotFound.tsx
+++ src/views/Search/ProductsNotFound/ProductsNotFound.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import NotFound from "components/common/NotFound";
 import styles from "./ProductsNotFound.module.scss";
+import Button from "components/common/Button";
+import { useNavigate } from "react-router-dom";
 
 const Search: React.FunctionComponent = () => {
+  const navigate = useNavigate();
   return (
     <NotFound
       title="No encontramos un producto que coincida con tu búsqueda"
       imgSrc="/product-not-found.jpg"
     >
       <div>
-        Navega por las&nbsp;
-        <a className={styles.a} href="/">
+        Navega por las
+        <div className={styles.categories}>
+        <Button color="white"  action={() => navigate("/")}>
           categorías
-        </a>
-        &nbsp;para encontrar un producto similar
+        </Button>
+        </div>
+        para encontrar un producto similar
       </div>
     </NotFound>
   );
