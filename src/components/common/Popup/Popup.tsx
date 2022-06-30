203def4d846278cb861f70c5eb05f10cfcce61de
Mark Rivera <markoctavio.riverac@gmail.com>
2022-06-30 18:44:01 -0500

    [DMP-427] Success Popup on signup

--- src/components/common/Popup/Popup.tsx
+++ src/components/common/Popup/Popup.tsx
@@ -1,9 +1,22 @@
 //import styles from "Popup.module.scss";
 
-interface PopupProps {}
+import { Dialog, DialogContent } from "@mui/material";
+import styles from "./Popup.module.scss";
 
-const Popup: React.FC<PopupProps> = () => {
-  return null;
+interface PopupProps {
+  open: boolean;
+  onClose: () => void;
+  content: React.ReactNode;
+}
+
+const Popup: React.FC<PopupProps> = ({ open, onClose, content }) => {
+  return (
+    <Dialog onClose={onClose} open={open}>
+      <DialogContent>
+        <div className={styles.container}>{content}</div>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default Popup;
