35fc2c676a34f3d4d630596adb67cec01184d6d8
dianabasilio <dianabasilio99@gmail.com>
2022-07-04 15:54:09 -0500

    [DMP-445] Added email and additional email validations

--- src/components/Form/TextField/TextField.tsx
+++ src/components/Form/TextField/TextField.tsx
@@ -2,6 +2,7 @@ import { Field } from "forms/form.types";
 import { useState } from "react";
 import { useFormContext } from "react-hook-form";
 import styles from "./TextField.module.scss";
+import validator from "validator";
 
 const TextField: React.FC<Field> = ({
   name,
@@ -12,15 +13,36 @@ const TextField: React.FC<Field> = ({
   type,
   disabled,
 }) => {
+  const [error, setError] = useState("");
   const methods = useFormContext();
   const [currentValue, setCurrentValue] = useState(value);
+  const validateEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const email = e.target.value;
+
+    if (validator.isEmail(email)) {
+      setError("");
+    } else {
+      setError("Favor de ingresar un correo valido");
+    }
+  };
+  const validateDeAceroEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const email = e.target.value;
+    const isDeAceroEmail = email.slice(-11) === "deacero.com";
+    if (validator.isEmail(email) && isDeAceroEmail) {
+      setError("");
+    } else {
+      setError("Favor de ingresar un correo de DEACERO");
+    }
+  };
+  
+  let errorStyle = error === "" ? "" : "input-error";
 
   return (
     <div className={styles.container}>
       <label>{label}</label>
       <input
         {...methods.register(name, { value, required })}
-        className={styles.input}
+        className={`${styles.input} ${styles[errorStyle]}`}
         defaultValue={currentValue}
         type={type}
         placeholder={placeholder}
@@ -28,8 +50,14 @@ const TextField: React.FC<Field> = ({
         onChange={(e) => {
           const value = e.target.value;
           setCurrentValue(value);
+          name === "additionalEmail" && validateEmail(e);
+          name === "email" && validateDeAceroEmail(e);
         }}
       />
+      {methods.formState.errors[name] && methods.formState.errors[name].type === "required" && (
+          <span className={styles["error-text"]}>Este campo es requerido</span>
+        )}
+      {error !== "" && <div className={styles["error-text"]}> {error}</div>}
     </div>
   );
 };
