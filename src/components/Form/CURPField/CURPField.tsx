6fd9003b2649d6cb48c1b0f886f1c8ce7a8ec165
Mark Rivera <markoctavio.riverac@gmail.com>
2022-07-11 12:51:21 -0500

    feat: [DMP-466] Add CURP field

---
+++ src/components/Form/CURPField/CURPField.tsx
@@ -0,0 +1,55 @@
+import { OptionsField } from "forms/form.types";
+import { useState } from "react";
+import { useFormContext } from "react-hook-form";
+import styles from "./CURPField.module.scss";
+
+const CURPField: React.FC<OptionsField> = ({
+  name,
+  value,
+  placeholder,
+  label,
+  required,
+  type,
+  disabled,
+}) => {
+  const [error] = useState("");
+  const methods = useFormContext();
+  const [currentValue, setCurrentValue] = useState(value);
+
+  const noError = error !== "";
+  const emptyFieldWhenRequired =
+    methods.formState.errors[name] &&
+    methods.formState.errors[name].type === "required";
+  const valueNotChanged = currentValue === value;
+
+  const errorStyle =
+    noError || (emptyFieldWhenRequired && valueNotChanged) ? "input-error" : "";
+
+  const requiredMessage = emptyFieldWhenRequired && valueNotChanged && (
+    <span className={styles["error-text"]}>Este campo es requerido</span>
+  );
+
+  noError && (methods.formState.errors[name] = error);
+
+  return (
+    <div className={styles.container}>
+      <label>{label}</label>
+      <input
+        {...methods.register(name, { value, required })}
+        className={`${styles.input} ${styles["input-rfc"]} ${styles[errorStyle]}`}
+        defaultValue={currentValue}
+        type={type}
+        placeholder={placeholder}
+        disabled={disabled}
+        onChange={(e) => {
+          const value = e.target.value;
+          setCurrentValue(value);
+        }}
+      />
+      {requiredMessage}
+      {error !== "" && <div className={styles["error-text"]}> {error}</div>}
+    </div>
+  );
+};
+
+export default CURPField;
