a0d345a1a7a3b5584769bf738a33940bd11a6f05
dianabasilio <dianabasilio99@gmail.com>
2022-07-12 18:55:37 -0500

    [DMP-462] Validation for when just other valid erros are present

--- src/components/Form/PasswordField/PasswordField.tsx
+++ src/components/Form/PasswordField/PasswordField.tsx
@@ -58,7 +58,7 @@ const PasswordField: React.FC<OptionsField> = ({
     <span className={styles["error-text"]}>Este campo es requerido</span>
   );
 
-  activeError && (methods.formState.errors[name] = error);
+  activeError && (methods.formState.errors[name] = {type: 'validation', message: error});
 
   return (
     <div className={styles.container}>
