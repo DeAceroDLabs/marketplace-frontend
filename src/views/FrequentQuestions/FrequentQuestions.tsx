c6a305fcb808d5fcbaa73ceb6bdba5b8f6307128
dianabasilio <dianabasilio99@gmail.com>
2022-07-19 16:47:58 -0500

    [DMP-453] Questions are now in a json.

--- src/views/FrequentQuestions/FrequentQuestions.tsx
+++ src/views/FrequentQuestions/FrequentQuestions.tsx
@@ -1,71 +1,25 @@
 import React from "react";
 import styles from "./FrequentQuestions.module.scss";
+import data from "./data";
 import Section from "components/common/Section";
 import ToogleCategories from "components/common/ToogleCategories";
 
 const FrequentQuestions: React.FunctionComponent = () => {
   return (
-    <Section title="Preguntas frecuentes">
-      <ToogleCategories title="Categoría" isOpen={true}>
-        <div className={styles["question-content-container"]}>
-          <div className={styles.question}>Pregunta 1</div>
-          <div className={styles.content}>
-            Li Europan lingues es membres del sam familie. Lor separat
-            existentie es un myth. Por scientie, musica, sport etc., li tot
-            Europa usa li sam vocabularium. Li lingues differe solmen in li
-            grammatica, li pronunciation e li plu commun vocabules. Omnicos
-            directe al desirabilit? de un nov lingua franca: on refusa continuar
-            payar custosi traductores. It solmen va esser necessi far uniform
-            grammatica, pronunciation e plu sommun paroles.{" "}
-          </div>
-        </div>
-      </ToogleCategories>
-
-      <ToogleCategories title="Categoría">
-        <div className={styles["question-content-container"]}>
-          <div className={styles.question}>Pregunta 2</div>
-          <div className={styles.content}>
-            Li Europan lingues es membres del sam familie. Lor separat
-            existentie es un myth. Por scientie, musica, sport etc., li tot
-            Europa usa li sam vocabularium. Li lingues differe solmen in li
-            grammatica, li pronunciation e li plu commun vocabules. Omnicos
-            directe al desirabilit? de un nov lingua franca: on refusa continuar
-            payar custosi traductores. It solmen va esser necessi far uniform
-            grammatica, pronunciation e plu sommun paroles.{" "}
-          </div>
-        </div>
-      </ToogleCategories>
-      <ToogleCategories title="Categoría">
-        <div className={styles["question-content-container"]}>
-          <div className={styles.question}>Pregunta 3</div>
-          <div className={styles.content}>
-            Li Europan lingues es membres del sam familie. Lor separat
-            existentie es un myth. Por scientie, musica, sport etc., li tot
-            Europa usa li sam vocabularium. Li lingues differe solmen in li
-            grammatica, li pronunciation e li plu commun vocabules. Omnicos
-            directe al desirabilit? de un nov lingua franca: on refusa continuar
-            payar custosi traductores. It solmen va esser necessi far uniform
-            grammatica, pronunciation e plu sommun paroles.{" "}
-          </div>
-        </div>
-      </ToogleCategories>
-      <div className={styles["last-question"]}>
-        <ToogleCategories title="Categoría">
-          <div className={styles["question-content-container"]}>
-            <div className={styles.question}>Pregunta 4</div>
-            <div className={styles.content}>
-              Li Europan lingues es membres del sam familie. Lor separat
-              existentie es un myth. Por scientie, musica, sport etc., li tot
-              Europa usa li sam vocabularium. Li lingues differe solmen in li
-              grammatica, li pronunciation e li plu commun vocabules. Omnicos
-              directe al desirabilit? de un nov lingua franca: on refusa
-              continuar payar custosi traductores. It solmen va esser necessi
-              far uniform grammatica, pronunciation e plu sommun paroles.{" "}
-            </div>
-          </div>
-        </ToogleCategories>
-      </div>
-    </Section>
+    <div className={styles["question-container"]}>
+      <Section title="Preguntas frecuentes">
+        {data.map((data, key) => {
+          return (
+            <ToogleCategories title={data.title} isOpen={data.isOpen}>
+              <div key={key} className={styles["question-content-container"]}>
+                <div className={styles.question}>{data.question}</div>
+                <div className={styles.content}>{data.content}</div>
+              </div>
+            </ToogleCategories>
+          );
+        })}
+      </Section>
+    </div>
   );
 };
 
---
+++ src/views/FrequentQuestions/data.tsx
@@ -0,0 +1,37 @@
+const data = [
+    {
+      "title": "Categoría",
+      "question": "Pregunta 1",
+      "content": "Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc., \
+      li tom Europa usa li sam vocabularium. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules. Omnicosdirecte \
+      al desirabilit? de un nov lingua franca: on refusa continuar payar custosi traductores. It solmen va esser necessi far uniform \
+      grammatica, pronunciation e plu sommun paroles.",
+      "isOpen": true,
+    },
+    {
+      "title": "Categoría",
+      "question": "Pregunta 2",
+      "content": "Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc., \
+      li tom Europa usa li sam vocabularium. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules. Omnicosdirecte \
+      al desirabilit? de un nov lingua franca: on refusa continuar payar custosi traductores. It solmen va esser necessi far uniform \
+      grammatica, pronunciation e plu sommun paroles.",
+    },
+    {
+      "title": "Categoría",
+      "question": "Pregunta 3",
+      "content": "Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc., \
+      li tom Europa usa li sam vocabularium. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules. Omnicosdirecte \
+      al desirabilit? de un nov lingua franca: on refusa continuar payar custosi traductores. It solmen va esser necessi far uniform \
+      grammatica, pronunciation e plu sommun paroles.",
+    },
+    {
+      "title": "Categoría",
+      "question": "Pregunta 4",
+      "content": "Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc., \
+      li tom Europa usa li sam vocabularium. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules. Omnicosdirecte \
+      al desirabilit? de un nov lingua franca: on refusa continuar payar custosi traductores. It solmen va esser necessi far uniform \
+      grammatica, pronunciation e plu sommun paroles.",
+    },
+  ];
+
+export default data;
