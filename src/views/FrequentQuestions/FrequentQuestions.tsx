87b8ec1351a5ddd7ff71ce992ea71775157e1840
dianabasilio <dianabasilio99@gmail.com>
2022-07-20 11:53:17 -0500

    [DMP-453] Scroll up when clicking button.

--- src/views/FrequentQuestions/FrequentQuestions.tsx
+++ src/views/FrequentQuestions/FrequentQuestions.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { useEffect } from "react";
 import styles from "./FrequentQuestions.module.scss";
 import data from "./data";
 import Section from "components/common/Section";
 import ExpandableContent from "components/common/ExpandableContent";
 
 const FrequentQuestions: React.FunctionComponent = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
   return (
     <div className={styles["question-container"]}>
       <Section title="Preguntas frecuentes">
