a9a64a67852bebb150d5ac62e059f7d7869faec6
Mark Rivera <markoctavio.riverac@gmail.com>
2022-07-21 11:14:51 -0500

    [DMP-449] Create User API

--- src/config/user.api.types.ts
+++ src/config/user.api.types.ts
@@ -1,8 +1,33 @@
 export const USER_API = {
-  createAuth0User: `${process.env.REACT_APP_AUTH0_APP_URL}/dbconnections/signup`,
+  createUser: `user`,
 };
 
 export type User = {
   email: string;
   password: string;
+};
+
+export type CreateUser = {
+  user: {
+    name: string;
+    lastName: string;
+    rfc: string;
+    curp: string;
+    phone: string;
+    fiscalDoc: string;
+    email: string;
+    additionalEmail: string;
+    password: string;
+  };
+  location: {
+    country: number;
+    zipCode: number;
+    state: number;
+    city: number;
+    neighborhood: number;
+    street: string;
+    externalNumber: number;
+    lat: number;
+    lon: number;
+  };
 };
